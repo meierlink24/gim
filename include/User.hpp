b3852793647d9a63c173fba05128d2500ef80d2c
Meier <meier.link24@gmail.com>
2025-03-29 16:02:40 +0000

    user funcs

---
+++ include/User.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <string.h>
+
+class User {
+  public: 
+    
+      // Switching between modes inside the text editor
+      void selectMode(char& command);
+      void inputMode(char& command);
+      void controlMode(std::string& command);
+      
+      
+                           
+
+
+}; 
